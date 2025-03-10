b29eec02ebce16a5d5447f3f8b8085127739d879
rajdavee <rajdave710@gmail.com>
2025-03-10 07:33:28 +0000

    raj

--- web/src/layouts/components/right-toolbar/index.tsx
+++ web/src/layouts/components/right-toolbar/index.tsx
@@ -1,101 +1,48 @@
-import { CircleHelp, MoonIcon, SunIcon } from 'lucide-react';
-
+import { MoonIcon, SunIcon } from 'lucide-react';
 import React from 'react';
-
-import { Space } from 'antd';
-
+import { Space, Button } from 'antd';
 import User from '../user';
-
 import { useTheme } from '@/components/theme-provider';
-
 import styled from './index.less';
 
-
-
 const Circle = ({ children, ...restProps }: React.PropsWithChildren) => {
-
   return (
-
     <div {...restProps} className={styled.circle}>
-
       {children}
-
     </div>
-
   );
-
 };
-
-
-
-const handleDocHelpClick = () => {
-
-  window.open('https://ragflow.io/docs/dev/category/guides', 'target');
-
-};
-
-
 
 const RightToolBar = () => {
-
   const { setTheme, theme } = useTheme();
 
-
-
   const onMoonClick = React.useCallback(() => {
-
     setTheme('light');
-
   }, [setTheme]);
-
-
 
   const onSunClick = React.useCallback(() => {
-
     setTheme('dark');
-
   }, [setTheme]);
 
-
-
   return (
-
     <div className={styled.toolbarWrapper}>
-
       <Space wrap size={16} className={styled.toolbarSpace}>
-
         <Circle>
-
           {theme === 'dark' ? (
-
             <MoonIcon onClick={onMoonClick} size={20} />
-
           ) : (
-
             <SunIcon onClick={onSunClick} size={20} />
-
           )}
-
         </Circle>
-
-        <Circle>
-
-          <CircleHelp className="size-4" onClick={handleDocHelpClick} />
-
-        </Circle>
-
+        
+        <Button type="primary" onClick={() => window.open('http://env-2466098.jcloud-ver-jpe.ik-server.com:8501/', '_blank')}>Chatbot</Button>
+        <Button type="default" onClick={() => window.open('http://env-2466098.jcloud-ver-jpe.ik-server.com:8502/', '_blank')}>Audio</Button>
+        
         <User />
-
       </Space>
-
     </div>
-
   );
-
 };
 
-
-
 export default RightToolBar;
-
 
